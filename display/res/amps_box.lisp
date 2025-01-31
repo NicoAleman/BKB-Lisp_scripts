dc7272c289b540436f50284c0959f305f963b8b7
Nico Aleman <NicoAleman1@hotmail.com>
2025-01-30 17:24:04 -0800

    Fix & Clean-up Trip + Current Readings
    - Scale of Trip Distance fixed (may be incorrect elsewhere? Worth investigating)
    - Formatting of Distance fixed to be more legible
    - Formatting of Amps fixed to be more legible

--- display/res/amps_box.lisp
+++ display/res/amps_box.lisp
@@ -1,9 +1,7 @@
 @const-start
 (defun write_amps (amp px py){
        (def amp_box (img-buffer 'indexed2 55 14))
-       (setvar 'amp (to-i (* 10 amp)))
-       (txt-block-l amp_box 1 0 0 font_9x14 (str-from-n amp "%04dA"))
-       (txt-block-c amp_box 1 28 0 font_9x14 ".")
+       (txt-block-l amp_box 1 0 0 font_9x14 (str-from-n amp "%3.0fA")) ;; Could set to %1.0fA or %2.0fA if we want to be more left aligned more often
        (disp-render amp_box px py '(0 0xFFFFFF))
 })
 @const-end
