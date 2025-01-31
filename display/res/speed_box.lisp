6a705954cdde35fbfc771ef4c4d778984645cfd4
Nico Aleman <NicoAleman1@hotmail.com>
2025-01-30 18:05:58 -0800

    Speed Reading Revamp
    Re-did formatting and layout to make the speed reading much more legible.
    - Made fraction digits spaced further from whole digits
    - Got rid of leading 0's, center text instead when < 10
    - Remove decimal beyond 100, and handle max of 999
    - Adjust mode_box to accommodate new layout
    
    TO-DO: Add option to disable fraction (only show whole number, round down)

--- display/res/speed_box.lisp
+++ display/res/speed_box.lisp
@@ -14,18 +14,38 @@
 })
 
 (defun write-speed (speed m_k px py color){
-
-    (def speed_box (img-buffer 'indexed4 60 30))
-    (setq speed (to-i (* 10 speed)))
-    (txt-block-l speed_box color 0 0 font_20x30 (str-from-n speed "%03d"))
-    (txt-block-c speed_box color 40 0 font_20x30 ".")
+    (def speed_box (img-buffer 'indexed4 65 30))
+    
+    ; Cap speed at 999.9
+    (if (>= speed 1000.0)
+        (setq speed 999.9)
+    )
+    
+    (if (>= speed 100.0)
+        (txt-block-l speed_box color 2 0 font_20x30 
+            (str-from-n (to-i speed) "%3d"))
+        (progn
+            ; Write whole number part - right aligned
+            (txt-block-l speed_box color 
+                (if (>= speed 10.0) 0 10) 0 font_20x30
+                (str-from-n (to-i speed) 
+                    (if (>= speed 10.0) "%2d" "%1d")))
+            ; Write decimal point
+            (txt-block-l speed_box color 
+                (if (>= speed 10.0) 34 24) 0 font_20x30 ".") ; Adjust decimal point position
+            ; Write fraction
+            (txt-block-l speed_box color 
+                (if (>= speed 10.0) 47 37) 0 font_20x30 ; Adjust fraction position
+                (str-from-n (to-i (* (- speed (to-i speed)) 10)) "%1d"))
+        )
+    )
+    
     (disp-render speed_box px py '(0x00000 0xFFFFFF 0x00FF00 0xFF0000))
     (def unit_box (img-buffer 'indexed2 27 17))
     (if (= m_k 0)
             (txt-block-l unit_box 1 0 0 font_9x14 "mph")
             (txt-block-l unit_box 1 0 0 font_9x14 "kph")
     )
-    (disp-render unit_box (+ px 63) (+ py 15) '(0x00000 0xFFFFFF))
-
+    (disp-render unit_box (+ px 68) (+ py 15) '(0x00000 0xFFFFFF))
 })
 @const-end
