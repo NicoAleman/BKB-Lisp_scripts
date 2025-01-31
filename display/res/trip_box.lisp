2d8dfb11ac3c794c1dacd13bd1491ce2de2f4250
Nico Aleman <NicoAleman1@hotmail.com>
2025-01-30 17:24:04 -0800

    Fix & Clean-up Trip + Current Readings
    - Scale of Trip Distance fixed (may be incorrect elsewhere? Worth investigating)
    - Formatting of Distance fixed to be more legible
    - Formatting of Amps fixed to be more legible

--- display/res/trip_box.lisp
+++ display/res/trip_box.lisp
@@ -3,17 +3,16 @@
 
        (def trip_box (img-buffer 'indexed2 64 14))
 
-       (if (= km_mi 0){
-            (setq dist (* dist 0.6213))
-            (setq dist (to-i dist))
-            (txt-block-l trip_box 1 0 0 font_9x14 (str-from-n dist "%05dmi"))
-       }
-       {
-            (setq dist (to-i dist))
-            (txt-block-l trip_box 1 0 0 font_9x14 (str-from-n dist "%05dkm"))
-       })
+       (setq dist (* dist 0.00001)) ;; Not sure why dist is scaled down so much? This fixes scale.
 
-       (txt-block-l trip_box 1 14 0 font_9x14 ".")
+       (if (= km_mi 0)
+            (setq dist (* dist 0.6213)))
+       (txt-block-l trip_box 1 0 0 font_9x14 
+           (str-from-n dist 
+               (if (= km_mi 0)
+                   (if (> dist 999.9) "999.9mi" "%5.1fmi")
+                   (if (> dist 999.9) "999.9km" "%5.1fkm"))))
+
        (disp-render trip_box px py '(0 0xFFFFFF))
 })
 @const-end
