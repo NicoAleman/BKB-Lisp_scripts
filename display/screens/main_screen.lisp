aa81f12e6083e0a02891465cfeef21e2914d1071
Nico Aleman <nicoaleman1@hotmail.com>
2025-02-03 01:56:21 -0800

    Minor Display Optimizations (Defined Refresh Rate + Remote SOC rate limiting)
    - Main Screen follows a defined refresh rate (currently a responsive 10Hz)
    - Remote SOC updated every 4 seconds except when charging, though this may be removed if SOC noise filter is added
    - Definitely optimize other components to only update when needed (including dont update speed when at a stop)

--- display/screens/main_screen.lisp
+++ display/screens/main_screen.lisp
@@ -1,31 +1,63 @@
+(def soc_update_prescaler 40)
+(def SOC_UPDATE_INTERVAL 40) ; Refresh every 40 loops (4 sec @ 10Hz)
+(def last_screen_update 0)
+(def SCREEN_REFRESH_INTERVAL 100) ; 100ms = 10Hz
+(def last_charging_state 0)
+
 @const-start
 (defun draw_main_screen(){
-    (def speed_color)
-    ;; ;; *EDIT - REMOVED GREEN COLOR FOR THROTTLE ENABLED*
-    ;; (if(= throttle_status 1)
-    ;;     (setq speed_color 2)
-    ;;     (setq speed_color 1)
-    ;; )
-    (setq speed_color 1)
-    (write_mode torq_mode (+ x_offset 2) (+ y_offset 20))
+    ; Return if not enough time has passed (elapsed time is less than our interval)
+    (if (> SCREEN_REFRESH_INTERVAL (- (systime) last_screen_update))
+        nil
+        (progn
+            (setq last_screen_update (systime))
+            
+            (def speed_color)
+            ;; ;; *EDIT - REMOVED GREEN COLOR FOR THROTTLE ENABLED*
+            ;; (if(= throttle_status 1)
+            ;;     (setq speed_color 2)
+            ;;     (setq speed_color 1)
+            ;; )
+            (setq speed_color 1)
+            (write_mode torq_mode (+ x_offset 2) (+ y_offset 20))
 
-    (if (> (secs-since last_peer_packet) 1)
-       (setq pairing_status 0)
+            (if (> (secs-since last_peer_packet) 1)
+               (setq pairing_status 0)
+            )
+            (write_online pairing_status (+ x_offset 95) (+ y_offset 19))
+            (write_direction direction (+ x_offset 59) (+ y_offset 0))
+            
+            ; Update Remote SOC when: interval reached, charging, or charging state changed
+            (def should_update (or (>= soc_update_prescaler SOC_UPDATE_INTERVAL)
+                                 (= (isCharging) 1)
+                                 (!= (isCharging) last_charging_state)))
+            
+            (if should_update
+                (progn
+                    (bat_soc (read_SOC) 2.5 4.25 0 1 (+ x_offset 85) (+ y_offset 0))
+                    (setq soc_update_prescaler 0)
+                )
+            )
+            (setq last_charging_state (isCharging))
+            (setq soc_update_prescaler (+ soc_update_prescaler 1))
+
+            (setq vin_min (* batt_type_config 3.0))
+            (setq vin_max (* batt_type_config 4.2))
+            (bat_soc vin vin_min vin_max 0 0 (+ x_offset 8) (+ y_offset 0))
+
+            (write_amps I_motor (+ x_offset 3) (+ y_offset 50))
+            
+            (if (= UNITS 1)
+                (progn
+                    (write-speed (speed_cal) 1 (+ x_offset 33) (+ y_offset 19) speed_color)
+                    (write_trip distance 1 (+ x_offset 63) (+ y_offset 50))
+                )
+                (progn
+                    (write-speed (* (speed_cal) 0.621) 0 (+ x_offset 28) (+ y_offset 19) speed_color)
+                    (write_trip distance 0 (+ x_offset 63) (+ y_offset 50))
+                )
+            )
+        )
     )
-    (write_online pairing_status (+ x_offset 95) (+ y_offset 19))
-    (write_direction direction (+ x_offset 59) (+ y_offset 0))
-    (bat_soc (read_SOC) 2.5 4.25 0 1 (+ x_offset 85) (+ y_offset 0))
-    (setq vin_min (* batt_type_config 3.0)); set min and max voltage according series cells
-    (setq vin_max (* batt_type_config 4.2))
-    (bat_soc vin vin_min vin_max 0 0  (+ x_offset 8) (+ y_offset 0))
-    (write_amps I_motor (+ x_offset 3) (+ y_offset 50))
-    (if (= UNITS 1){
-         (write-speed (speed_cal) 1 (+ x_offset 33) (+ y_offset 19) speed_color)          ; km
-         (write_trip distance 1 (+ x_offset 63) (+ y_offset 50))
-    }
-    {
-        (write-speed (* (speed_cal) 0.621) 0 (+ x_offset 28) (+ y_offset 19) speed_color)  ; miles
-        (write_trip distance 0 (+ x_offset 63) (+ y_offset 50))
-    })
 })
 @const-end
